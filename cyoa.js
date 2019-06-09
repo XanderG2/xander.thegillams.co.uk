cf2cdd3f0873780524dbcbb5fd32e54f732875a7
Xander Gillam <XanderG2@users.noreply.github.com>
2019-06-09 13:16:08 +0100

    [Daddy] State management

--- cyoa.js
+++ cyoa.js
@@ -2,10 +2,37 @@
 const logEl = document.getElementById("log");
 const buttonsEl = document.getElementById("buttons");
 
-function reset() {
+let state;
+try {
+  state = JSON.parse(localStorage.getItem("state"));
+} catch (e) {
+  // Nothing;
+}
+
+let lastStateString = null;
+function saveState() {
+  const newStateString = JSON.stringify(state);
+  if (newStateString !== lastStateString) {
+    localStorage.setItem("state", newStateString);
+    lastStateString = newStateString;
+  }
+}
+
+function load() {
+  if (!state) {
+    state = { pageName: "start" };
+  }
   logEl.innerHTML = "";
   setActions([]);
+  go(state.pageName || "start");
+  saveState();
 }
+
+function restart() {
+  state = null;
+  load();
+}
+window.restart = restart;
 
 function addLog(html) {
   const divEl = document.createElement("div");
@@ -19,7 +46,10 @@ function setActions(buttons) {
   if (buttons.length) {
     for (const button of buttons) {
       const buttonEl = document.createElement("button");
-      buttonEl.onclick = button.action;
+      buttonEl.onclick = () => {
+        button.action();
+        saveState();
+      };
       buttonEl.textContent = button.text;
       buttonsEl.appendChild(buttonEl);
     }
@@ -28,17 +58,16 @@ function setActions(buttons) {
   }
 }
 
-reset();
-
 /* Xander */
 const story = {
   start: {
-    message: "welcome to the story!",
+    message:
+      "you awaken in a dark room. By the bed is an enchient, dusty lamp. The door way leeds to a corridor.",
     actions: [
       {
-        text: "hi",
-        action: function() {
-          goToPage("hi");
+        text: "go to corridor",
+        action() {
+          go("corridor");
         },
       },
     ],
@@ -49,23 +78,50 @@ const story = {
     actions: [
       {
         text: "go back to start",
-        action: function() {
-          reset();
-          goToPage("start");
+        action() {
+          restart();
+        },
+      },
+    ],
+  },
+  corridor: {
+    message:
+      "you come to the corridor. You have two options one go left one go right wich one do you choose?",
+    actions: [
+      {
+        text: "go left",
+      },
+      {
+        text: "go right",
+        action() {
+          go("right");
+        },
+      },
+    ],
+  },
+  right: {
+    message:
+      "you go right you see one more door. You can either go back or go into the other door. Do you choose to keep going or not?",
+    actions: [
+      {
+        text: "go back",
+        action() {
+          go("corridor");
         },
       },
     ],
   },
 };
 
-function goToPage(pageName) {
+function go(pageName) {
   const page = story[pageName];
   if (!page) {
     alert(`page ${pageName} not found`);
     return;
   }
+  state.pageName = pageName;
   addLog(page.message);
   setActions(page.actions);
 }
 
-goToPage("start");
+load();
