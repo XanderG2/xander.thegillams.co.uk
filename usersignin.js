d0603f1b779ea84d069c5121d7d8e5ca4ac7e45e
XanderG2 <XanderG2@users.noreply.github.com>
2020-07-07 12:38:24 +0100

    Update usersignin.js

--- usersignin.js
+++ usersignin.js
@@ -1,6 +1,6 @@
 function check(form) {
   if (form.userid.value == "xander" && form.pswrd.value == "5113") {
-    window.open("https://www.youtube.com");
+    window.open("https://xander.thegillams.co.uk/xandersAccount.html");
   }
   if (form.userid.value == "Daddy" && form.pswrd.value == "Ilovemummy") {
     window.open("https://youtube.com");
