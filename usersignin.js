9f090d61724a44dd1c36746bccc5d0750318c8fb
XanderG2 <XanderG2@users.noreply.github.com>
2020-07-07 12:37:37 +0100

    Sign in page for me

---
+++ usersignin.js
@@ -0,0 +1,15 @@
+function check(form) {
+  if (form.userid.value == "xander" && form.pswrd.value == "5113") {
+    window.open("https://www.youtube.com");
+  }
+  if (form.userid.value == "Daddy" && form.pswrd.value == "Ilovemummy") {
+    window.open("https://youtube.com");
+  }
+  if (form.userid.value == "chicken" && form.pswrd.value == "bcark") {
+    window.open("https://youtube.com");
+  }
+  if (form.userid.value == "Mummy" && form.pswrd.value == "Ilovedaddy") {
+    window.open("https://youtube.com");
+  }
+  }
+}
