83cef08275e695b22b9dbd0ebc168942662b3207
XanderG2 <XanderG2@users.noreply.github.com>
2020-07-11 16:59:28 +0100

    Update Pong.js

--- Pong/Pong.js
+++ Pong/Pong.js
@@ -6,67 +6,84 @@ let point2 = 0;
 let myScore;
 let myScore1;
 let ctx;
+const fps = 30;
 
-window.startGame = function startGame() {
+const P1_UP = 87; // 'w'
+const P1_DOWN = 83; // 's'
+const P2_UP = 38; //Up arrow
+const P2_DOWN = 40; // Down arrow
+
+const BALL_SPEED_X = 8;
+const BALL_SPEED_Y = 4;
+const HOCKEY_MOVEMENT = 10;
+const WIN_SCORE = 5;
+
+window.startGame = () => {
   myGameArea.start();
-  hockey1 = new component(8, 60, "yellow", 20, 150);
-  hockey2 = new component(8, 60, "lime", 670, 150);
-  ball = new component(7, 7, "green", 350, 170);
-  myScore = new component("16px", "Consolas", "yellow", 200, 25, "text");
-  myScore1 = new component("16px", "Consolas", "lime", 410, 25, "text");
+  hockey1 = new Component(8, 60, "yellow", 20, 150);
+  hockey2 = new Component(8, 60, "lime", 670, 150);
+  ball = new Component(7, 7, "green", 350, 170);
+  ball.speedX = BALL_SPEED_X;
+  myScore = new Component("16px", "Consolas", "yellow", 200, 25, "text");
+  myScore1 = new Component("16px", "Consolas", "lime", 410, 25, "text");
 };
 
 const myGameArea = {
   canvas: document.createElement("canvas"),
-  start: function() {
+  keys: {},
+  start() {
     this.canvas.width = 700;
     this.canvas.height = 390;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 30);
-    window.addEventListener("keydown", function(e) {
-      myGameArea.keys = myGameArea.keys || [];
+    this.interval = setInterval(updateGameArea, Math.floor(1000 / fps));
+    window.addEventListener("keydown", e => {
       myGameArea.keys[e.keyCode] = true;
     });
-    window.addEventListener("keyup", function(e) {
+    window.addEventListener("keyup", e => {
       myGameArea.keys[e.keyCode] = false;
     });
   },
 
-  clear: function() {
+  clear() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
-  stop: function() {
+
+  stop() {
     clearInterval(this.interval);
   },
 };
 
-function component(width, height, color, x, y, type) {
-  this.type = type;
-  this.width = width;
-  this.height = height;
-  this.x = x;
-  this.y = y;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.update = function() {
+class Component {
+  constructor(width, height, color, x, y, type) {
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.color = color;
+  }
+
+  update() {
     ctx = myGameArea.context;
     if (this.type == "text") {
       ctx.font = this.width + " " + this.height;
-      ctx.fillStyle = color;
+      ctx.fillStyle = this.color;
       ctx.fillText(this.text, this.x, this.y);
     } else {
-      ctx.fillStyle = color;
+      ctx.fillStyle = this.color;
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-  };
+  }
 
-  this.newPos = function() {
+  newPos() {
     this.x += this.speedX;
     this.y += this.speedY;
-  };
+  }
 
-  this.crashWith = function(otherobj) {
+  crashWith(otherobj) {
     const myleft = this.x;
     const myright = this.x + this.width;
     const mytop = this.y;
@@ -85,10 +102,43 @@ function component(width, height, color, x, y, type) {
       crash = false;
     }
     return crash;
-  };
+  }
 }
 
 function updateGameArea() {
+  //Keyboard control
+
+  /* NOTE: if you hit the ball on a corner whilst
+   moving in that direction, the ball gets spinned. */
+  if (myGameArea.keys[P1_UP]) {
+    hockey1.y -= HOCKEY_MOVEMENT;
+    if (ball.crashWith(hockey1)) {
+      ball.speedY += -BALL_SPEED_Y;
+      ball.speedX = BALL_SPEED_X;
+    }
+  }
+  if (myGameArea.keys[P1_DOWN]) {
+    hockey1.y += HOCKEY_MOVEMENT;
+    if (ball.crashWith(hockey1)) {
+      ball.speedY += BALL_SPEED_Y;
+      ball.speedX = BALL_SPEED_X;
+    }
+  }
+  if (myGameArea.keys[P2_UP]) {
+    hockey2.y -= HOCKEY_MOVEMENT;
+    if (ball.crashWith(hockey2)) {
+      ball.speedY += -BALL_SPEED_Y;
+      ball.speedX = -BALL_SPEED_X;
+    }
+  }
+  if (myGameArea.keys[P2_DOWN]) {
+    hockey2.y += HOCKEY_MOVEMENT;
+    if (ball.crashWith(hockey2)) {
+      ball.speedY += BALL_SPEED_Y;
+      ball.speedX = -BALL_SPEED_X;
+    }
+  }
+
   //Hockey Control
 
   if (hockey1.y <= 0) {
@@ -104,65 +154,44 @@ function updateGameArea() {
     hockey2.y = 350;
   }
 
-  //Keyboard control
-
-  if (myGameArea.keys && myGameArea.keys[38]) {
-    hockey1.y -= 10;
-    if (ball.crashWith(hockey1)) {
-      ball.speedY = -4;
-      ball.speedX = 14;
-    }
-  }
-  if (myGameArea.keys && myGameArea.keys[40]) {
-    hockey1.y += 10;
-    if (ball.crashWith(hockey1)) {
-      ball.speedY = 4;
-      ball.speedX = 14;
-    }
-  }
-  if (myGameArea.keys && myGameArea.keys[37]) {
-    hockey2.y -= 10;
-    if (ball.crashWith(hockey2)) {
-      ball.speedY = -4;
-      ball.speedX = -8;
-    }
-  }
-  if (myGameArea.keys && myGameArea.keys[39]) {
-    hockey2.y += 10;
-    if (ball.crashWith(hockey2)) {
-      ball.speedY = 4;
-      ball.speedX = -8;
-    }
-  }
-
   //BALL MOVEMENTS
   ball.newPos();
 
   if (ball.crashWith(hockey1)) {
-    ball.speedY = 0;
-    ball.speedX = 13;
+    //ball.speedY = 0;
+    ball.speedX = BALL_SPEED_X;
   } else if (ball.crashWith(hockey2)) {
-    ball.speedY = 0;
-    ball.speedX = -8;
+    //ball.speedY = 0;
+    ball.speedX = -BALL_SPEED_X;
   } else {
-    ball.x += -4;
+    //ball.x += -4;
   }
 
   if (ball.y <= 0) {
-    ball.speedY = 4;
+    ball.speedY = Math.abs(ball.speedY);
   }
   if (ball.y >= 390) {
-    ball.speedY = -4;
+    ball.speedY = -Math.abs(ball.speedY);
   }
   if (ball.x <= 2) {
     ball.x = 690;
     point2 += 1;
-    //myGameArea.stop();
+    ball.speedY = 0;
+    ball.y = 170;
+    if (point2 >= WIN_SCORE) {
+      myGameArea.stop();
+      alert("P2 wins");
+    }
   }
   if (ball.x >= 700) {
     ball.x = 0;
     point1 += 1;
-    //myGameArea.stop();
+    ball.speedY = 0;
+    ball.y = 170;
+    if (point1 >= WIN_SCORE) {
+      myGameArea.stop();
+      alert("P1 wins");
+    }
   }
   // if you want
   //			console.log(point1);
