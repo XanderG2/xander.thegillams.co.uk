9e7b42e9d5cd9a0c098e8ebc74e0ca0eef6e3525
XanderG2 <XanderG2@users.noreply.github.com>
2020-07-19 12:45:29 +0100

    user messages and grandad account

--- usersignin.js
+++ usersignin.js
@@ -1,23 +1,26 @@
 function check(form) {
   if (form.userid.value == "xander" && form.pswrd.value == "5113") {
-    window.open("https://xander.thegillams.co.uk/xandersAccount.html");
+    window.open("xandersAccount.html");
   }
   if (form.userid.value == "Daddy" && form.pswrd.value == "Ilovemummy") {
-    window.open("https://xander.thegillams.co.uk/daddysAccount.html");
+    window.open("/daddysAccount.html");
   }
   if (form.userid.value == "chicken" && form.pswrd.value == "bcark") {
-    window.open("https://xander.thegillams.co.uk/zacksAccount.html");
+    window.open("zacksAccount.html");
   }
   if (form.userid.value == "Mummy" && form.pswrd.value == "Ilovedaddy") {
-    window.open("https://xander.thegillams.co.uk/mummysAccount.html");
+    window.open("mummysAccount.html");
   }
   if (form.userid.value == "Brontie" && form.pswrd.value == "145347") {
-    window.open("https://xander.thegillams.co.uk/TheCat.html");
+    window.open("TheCat.html");
   }
   if (form.userid.value == "Granny" && form.pswrd.value == "1234") {
-    window.open("https://xander.thegillams.co.uk/grannysAccount.html");
+    window.open("grannysAccount.html");
   }
   if (form.userid.value == "Captain" && form.pswrd.value == "609") {
-    window.open("https://xander.thegillams.co.uk/DogAccount.html");
+    window.open("DogAccount.html");
+  }
+  if (form.userid.value == "Grandad" && form.pswrd.value == "1111") {
+    window.open("grandadsAccount.html");
   }
 }
