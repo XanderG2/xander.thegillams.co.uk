4c40a8ad4b43599ec77b3b7ae8514061c6b68766
XanderG2 <XanderG2@users.noreply.github.com>
2020-07-28 11:58:01 +0100

    userExclusiv

--- UserStuffExclusive/usersignin.js
+++ UserStuffExclusive/usersignin.js
@@ -1,26 +1,26 @@
 function check(form) {
   if (form.userid.value == "xander" && form.pswrd.value == "5113") {
-    window.open("xandersAccount.html");
+    window.open("users/xandersAccount.html");
   }
   if (form.userid.value == "Daddy" && form.pswrd.value == "Ilovemummy") {
-    window.open("/daddysAccount.html");
+    window.open("users/daddysAccount.html");
   }
   if (form.userid.value == "chicken" && form.pswrd.value == "bcark") {
-    window.open("zacksAccount.html");
+    window.open("users/zacksAccount.html");
   }
   if (form.userid.value == "Mummy" && form.pswrd.value == "Ilovedaddy") {
-    window.open("mummysAccount.html");
+    window.open("users/mummysAccount.html");
   }
   if (form.userid.value == "Brontie" && form.pswrd.value == "145347") {
-    window.open("TheCat.html");
+    window.open("users/TheCat.html");
   }
   if (form.userid.value == "Granny" && form.pswrd.value == "1234") {
-    window.open("grannysAccount.html");
+    window.open("users/grannysAccount.html");
   }
   if (form.userid.value == "Captain" && form.pswrd.value == "609") {
-    window.open("DogAccount.html");
+    window.open("users/DogAccount.html");
   }
   if (form.userid.value == "Granddad" && form.pswrd.value == "1111") {
-    window.open("grandadsAccount.html");
+    window.open("users/grandadsAccount.html");
   }
 }
