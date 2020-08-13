0309d4b59096d2974574734428e7ec18f0a46ea8
XanderG2 <XanderG2@users.noreply.github.com>
2020-08-13 13:56:09 +0100

    index

---
+++ index_old.js
@@ -0,0 +1,10 @@
+$(function() {
+  $("#item").click(function() {
+    $("#submenu").slideToggle(500);
+  });
+});
+$(function() {
+  $("#itemtwo").click(function() {
+    $("#submenutwo").slideToggle(500);
+  });
+});
