bc3399f8bfcf56c752cc19ce3443316fcad66f65
XanderG2 <XanderG2@users.noreply.github.com>
2021-07-25 14:25:58 +0100

    Update Cipher.js

--- Cipher/Cipher.js
+++ Cipher/Cipher.js
@@ -1,20 +1,16 @@
 /* eslint-disable no-console */
-console.log("Thing");
-const message = "jryy qbar lbh penpxrq vg";
+
+const message = "yrggre ahzore o vf ahzore 7";
 const messageg = message.toUpperCase();
-const letters = messageg.split("");
-console.log(letters);
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".repeat(3);
-const alphabetletters = alphabet.split("");
-console.log(alphabetletters);
 
 let output = "";
-for (let i = 0; i < letters.length; i = i + 1) {
-  const l = letters[i];
-  const n = alphabetletters.indexOf(l);
+for (let i = 0; i < messageg.length; i = i + 1) {
+  const l = messageg[i];
+  const n = alphabet.indexOf(l);
   if (n >= 0) {
     const c = n + 13;
-    output = output + alphabetletters[c];
+    output = output + alphabet[c];
   } else {
     output = output + l;
   }
