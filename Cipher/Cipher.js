766983c852c0333a8b8bf95d09e87b20e5a294c8
XanderG2 <XanderG2@users.noreply.github.com>
2021-07-25 15:13:47 +0100

    cipher

--- Cipher/Cipher.js
+++ Cipher/Cipher.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-shadow */
 /* eslint-disable no-console */
 const ul = document.getElementById("ul");
-const message = "jub jnagf cevatyrf";
+const message = "twppm sv cmg ajw jwaxsnu htsi";
 const messageg = message.toUpperCase();
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".repeat(3);
 
@@ -10,21 +10,25 @@ function log(m) {
   el.innerText = m;
   ul.appendChild(el);
 }
-function decipher(message, rot) {
+function decipher(message, callback) {
   let output = "";
   for (let i = 0; i < message.length; i = i + 1) {
     const l = message[i];
     const n = alphabet.indexOf(l);
     if (n >= 0) {
-      const c = n + rot;
+      const c = callback(n);
       output = output + alphabet[c];
     } else {
       output = output + l;
     }
   }
-  let outputlowercase = output.toLowerCase();
+  const outputlowercase = output.toLowerCase();
   log(outputlowercase);
 }
 for (let j = 0; j < 26; j = j + 1) {
-  decipher(messageg, j);
+  decipher(messageg, function(n) {
+    return -n + 26 + j;
+  });
 }
+console.log("29x57 is");
+console.log(29 * 57);
