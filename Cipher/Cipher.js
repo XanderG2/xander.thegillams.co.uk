4f162039b954c417cc34244c741da2e1a9796dc5
XanderG2 <XanderG2@users.noreply.github.com>
2021-07-25 14:50:42 +0100

    made cipher print to html

--- Cipher/Cipher.js
+++ Cipher/Cipher.js
@@ -1,19 +1,30 @@
+/* eslint-disable no-shadow */
 /* eslint-disable no-console */
-
-const message = "yrggre ahzore o vf ahzore 7";
+const ul = document.getElementById("ul");
+const message = "jub jnagf cevatyrf";
 const messageg = message.toUpperCase();
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".repeat(3);
 
-let output = "";
-for (let i = 0; i < messageg.length; i = i + 1) {
-  const l = messageg[i];
-  const n = alphabet.indexOf(l);
-  if (n >= 0) {
-    const c = n + 13;
-    output = output + alphabet[c];
-  } else {
-    output = output + l;
-  }
+function log(m) {
+  const el = document.createElement("li");
+  el.innerText = m;
+  ul.appendChild(el);
 }
-let outputlowercase = output.toLowerCase();
-console.log(outputlowercase);
+function decipher(message, rot) {
+  let output = "";
+  for (let i = 0; i < message.length; i = i + 1) {
+    const l = message[i];
+    const n = alphabet.indexOf(l);
+    if (n >= 0) {
+      const c = n + rot;
+      output = output + alphabet[c];
+    } else {
+      output = output + l;
+    }
+  }
+  let outputlowercase = output.toLowerCase();
+  log(outputlowercase);
+}
+for (let j = 0; j < 26; j = j + 1) {
+  decipher(messageg, j);
+}
