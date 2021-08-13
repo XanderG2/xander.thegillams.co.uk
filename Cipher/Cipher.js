21b3f17db6bc98df84af58170dae157cd69ca46d
XanderG2 <XanderG2@users.noreply.github.com>
2021-08-13 11:18:20 +0100

    updated ciphers
    You can now use an input box to cipher and uncipher stuff

--- Cipher/Cipher.js
+++ Cipher/Cipher.js
@@ -1,34 +1,43 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
 /* eslint-disable no-shadow */
 /* eslint-disable no-console */
-const ul = document.getElementById("ul");
-const message = "twppm sv cmg ajw jwaxsnu htsi";
-const messageg = message.toUpperCase();
+window.ul = document.getElementById("ul");
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".repeat(3);
 
+let inputElm = document.querySelector("input");
+
+function buttonclicked() {
+  let inputValue = inputElm.value;
+  let messageg = inputValue.toUpperCase();
+  for (let j = 0; j < 26; j = j + 1) {
+    decipher(messageg, function(n) {
+      return -n + 26 + j;
+    });
+  }
+}
 function log(m) {
   const el = document.createElement("li");
   el.innerText = m;
-  ul.appendChild(el);
+  window.ul.appendChild(el);
 }
 function decipher(message, callback) {
-  let output = "";
+  window.output = "";
   for (let i = 0; i < message.length; i = i + 1) {
     const l = message[i];
     const n = alphabet.indexOf(l);
     if (n >= 0) {
       const c = callback(n);
-      output = output + alphabet[c];
+      window.output = window.output + alphabet[c];
     } else {
-      output = output + l;
+      window.output = window.output + l;
     }
   }
-  const outputlowercase = output.toLowerCase();
+  idk();
+}
+// eslint-disable-next-line no-unused-vars
+function idk() {
+  let output = window.output;
+  let outputlowercase = output.toLowerCase();
   log(outputlowercase);
 }
-for (let j = 0; j < 26; j = j + 1) {
-  decipher(messageg, function(n) {
-    return -n + 26 + j;
-  });
-}
-console.log("29x57 is");
-console.log(29 * 57);
