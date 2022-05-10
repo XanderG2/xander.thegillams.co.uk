6bca814b937dadb366e3b45bcc31ac81f09a4aea
XanderG2 <XanderG2@users.noreply.github.com>
2022-05-10 19:37:55 +0100

    flactory engine

---
+++ flactory/flactory.js
@@ -0,0 +1,83 @@
+let canvas;
+let x = 2;
+let y = 2;
+let zoom = 1;
+const MAXZOOM = 3;
+const MINZOOM = 0.25;
+const NORMALTILESTOSHOW = 10;
+function start() {
+  canvas = document.getElementById("canvas");
+  render();
+}
+
+function size() {
+  const screensize = Math.max(window.innerWidth, window.innerHeight);
+  const xOffset = Math.floor(window.innerWidth / 2);
+  const yOffset = Math.floor(window.innerHeight / 2);
+  const tilesize = (screensize / NORMALTILESTOSHOW) * zoom;
+  return { xOffset, yOffset, tilesize };
+}
+
+function render() {
+  let $ = canvas.getContext("2d");
+
+  canvas.width = window.innerWidth;
+
+  canvas.height = window.innerHeight;
+  const { xOffset, yOffset, tilesize } = size();
+
+  $.fillStyle = "#00AA00";
+  $.fillRect(0, 0, canvas.width, canvas.height);
+
+  $.fillStyle = "#000000";
+  $.fillRect(
+    xOffset + (2 - x) * tilesize,
+    yOffset + (2 - y) * tilesize,
+    tilesize,
+    -tilesize
+  );
+}
+// 0 = nothing, 2 = middle mouse drag
+let mousemode = 0;
+let mousex = 0;
+let mousey = 0;
+
+function mousemove(event) {
+  event.preventDefault();
+  if (mousemode === 2) {
+    const xdif = event.clientX - mousex;
+    const ydif = event.clientY - mousey;
+    mousex = event.clientX;
+    mousey = event.clientY;
+    const { tilesize } = size();
+    x -= xdif / tilesize;
+    y -= ydif / tilesize;
+    render();
+    //console.log(event);
+  }
+}
+function mousedown(event) {
+  event.preventDefault();
+  if (event.which === 2) {
+    mousemode = 2;
+    mousex = event.clientX;
+    mousey = event.clientY;
+  }
+}
+function mouseup(event) {
+  event.preventDefault();
+  mousemode = 0;
+}
+function wheel(event) {
+  if (event.deltaY > 0) {
+    zoom = Math.min(MAXZOOM, Math.max(MINZOOM, zoom / 1.25));
+  } else if (event.deltaY < 0) {
+    zoom = Math.min(MAXZOOM, Math.max(MINZOOM, zoom * 1.25));
+  }
+  render();
+}
+window.addEventListener("DOMContentLoaded", start);
+window.addEventListener("mousemove", mousemove);
+window.addEventListener("mousedown", mousedown);
+window.addEventListener("mouseup", mouseup);
+window.addEventListener("wheel", wheel);
