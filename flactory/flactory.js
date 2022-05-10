e1d184968d3bdc11e5fbbc66990ce15030a4af10
XanderG2 <XanderG2@users.noreply.github.com>
2022-05-10 20:11:19 +0100

    thats pretty much game built now dontcha think

--- flactory/flactory.js
+++ flactory/flactory.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 let canvas;
 let x = 2;
 let y = 2;
@@ -26,16 +27,29 @@ function render() {
   canvas.height = window.innerHeight;
   const { xOffset, yOffset, tilesize } = size();
 
+  const left = Math.floor(x - NORMALTILESTOSHOW / zoom / 2);
+  const bottom = Math.floor(y - NORMALTILESTOSHOW / zoom / 2);
+  const numberoftiles = Math.ceil(NORMALTILESTOSHOW / zoom + 2);
+  console.log(left, bottom);
   $.fillStyle = "#00AA00";
   $.fillRect(0, 0, canvas.width, canvas.height);
 
-  $.fillStyle = "#000000";
-  $.fillRect(
-    xOffset + (2 - x) * tilesize,
-    yOffset + (2 - y) * tilesize,
-    tilesize,
-    -tilesize
-  );
+  for (let a = 0; a < numberoftiles; a++) {
+    for (let b = 0; b < numberoftiles; b++) {
+      const tX = a + left;
+      const tY = b + bottom;
+      const c = (((tX + tY) % 3) + 3) % 3;
+      $.fillStyle = `rgb(${c === 0 ? 255 : 0}, ${c === 1 ? 255 : 0}, ${
+        c === 2 ? 255 : 0
+      })`;
+      $.fillRect(
+        xOffset + (tX - x) * tilesize,
+        yOffset + (tY - y) * tilesize,
+        tilesize,
+        -tilesize
+      );
+    }
+  }
 }
 // 0 = nothing, 2 = middle mouse drag
 let mousemode = 0;
