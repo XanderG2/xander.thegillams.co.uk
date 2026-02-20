016ea96bb973905d595d3aa19cee99bb6fa260e3
Xander <XanderG2@users.noreply.github.com>
2026-02-20 21:14:58 +0000

    Add a searching system

--- music/script.js
+++ music/script.js
@@ -8,3 +8,24 @@ document.querySelectorAll(".nacont").forEach(box => {
     text.classList.add("scrolling");
   }
 });
+
+// eslint-disable-next-line no-unused-vars
+function toggle(section) {
+  const sec = document.getElementById(section);
+  if (!sec) return;
+  if (sec.style.display != "none") sec.style.display = "none";
+  else if (sec.style.display == "none") sec.style.display = "grid";
+}
+
+function search() {
+  const toSearch = document.getElementById("genresAlbums").value.toLowerCase();
+  const els = document.querySelector(".albums").querySelectorAll(".music");
+  els.forEach(el => {
+    const genres = Array.from(el.querySelectorAll(".genre")).map(e => e.innerText.toLowerCase());
+    const contains = genres.some(txt => txt.includes(toSearch));
+    if (!contains) el.style.display = "none";
+    else el.style.display = "block";
+  });
+}
+
+document.getElementById("genresAlbums").addEventListener("keyup", search);
